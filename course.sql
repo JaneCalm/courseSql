c410acde928136acd3ca48e122c106c752e575a1
JaneCalm <drugsdream@gmail.com>
2019-11-18 01:53:30 +0300

    2

--- course.sql
+++ course.sql
@@ -1,9 +1,11 @@
 /*
- За основу данной базы, я взяла сайт https://www.toramp.com/.
+ За основу данной базы, я взяла сайт https://www.toramp.com/, полностью соблюдая их структуру.
  Акцент данного ресурса сделан на сериалах и возможности создавать свое расписание просмотров.
  Задачей базы данных является хранение актуальной информации о просмотренных сериях, оценках.
  На ее основе можно анализировать популярность сериалов, как по посещяемости страниц, так и по количеству людей просмотревших все серии сериала. 
  Также есть задача оптимизировать поиск интересующего сериала для пользователя, на основе сопутствующей информации. (по актерам, по создателям и тд)
+ По идее жанры можно было-бы хранить в множественном выборе SET, но тогда нужно четко определиться, что их будет не более 60+, а если их вдруг станет больше, то 
+ прийдется переделывать связи, поэтому я реализовала их также, как и остальные поисковые запросы
 */
 
 drop database if exists courseSql;
@@ -42,6 +44,142 @@ CREATE TABLE author (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL UNIQUE COMMENT 'создатель идеи'
 );
+
+drop table if exists users;
+create table users(
+	id SERIAL PRIMARY KEY,
+	firstname VARCHAR(100),
+	lastname VARCHAR(100),
+	email VARCHAR(120) not null unique,
+	password_u VARCHAR(100) not null,
+	gender CHAR(2),
+	birthday DATE,
+	INDEX (firstname, lastname)
+)COMMENT = 'пользователи';
+
+drop table if exists schedule;
+create table schedule(
+	 user_id serial PRIMARY KEY,
+	 delay ENUM('1', '2', '3', '4', '5', '6', '7','8', '9', '10') COMMENT 'задержка дней рассписания',
+	 opened ENUM('10', '11', '12', '13', '14', '15', '16','17', '18', '19', '20') COMMENT 'количество отображаемых серий', 
+	 loaded ENUM('5', '6', '7', '8', '9', '10', '11','12', '13', '14', '15') COMMENT 'количество подгружаемых серий', 
+	 foreign key (user_id) references users(id) ON DELETE CASCADE ON UPDATE CASCADE
+) COMMENT = 'общие настройки расписания пользователя';
+
+drop table if exists messages;
+create table messages(
+	id SERIAL PRIMARY KEY,
+	email VARCHAR(120) not null,
+	user_id BIGINT UNSIGNED unique,
+	body TEXT,
+	created_at DATETIME DEFAULT NOW(),
+	index(email),
+	foreign key (user_id) references users(id) ON DELETE set null ON UPDATE CASCADE
+) COMMENT = 'Сообщения отправленные из формы сайта';
+
+drop table if exists picture;
+create table picture(
+	id SERIAL PRIMARY KEY,
+	filename VARCHAR(255),
+	`size` int,
+	metadata JSON
+)COMMENT = 'картинки';
+
+DROP TABLE IF EXISTS serials;
+CREATE TABLE serials(
+  id SERIAL PRIMARY KEY,
+  picture_id BIGINT UNSIGNED,  
+  nameR VARCHAR(255) not NULL COMMENT 'Название на русском',
+  nameE VARCHAR(255) COMMENT 'Название на английском',
+  description TEXT COMMENT 'Описание',
+  created_at YEAR,
+  end_at YEAR,
+  reit DECIMAL (4,2) UNSIGNED COMMENT 'Рейтинг',
+  status ENUM('выходит', 'завершен', 'ждем премьеры', 'решается дальнейшая судьба проекта', 'вернется весной', 'вернется летом', 'вернется осенью','вернется зимой', 'вернется \'неизвестно\''),
+  timeon VARCHAR(255) COMMENT 'время выхода в эфир',
+  timeline int COMMENT 'длительность серии',
+  allepisods int UNSIGNED,
+  allseasons int UNSIGNED,
+  foreign key (picture_id) references picture(id) ON DELETE set null ON UPDATE CASCADE
+) COMMENT = 'Сериалы';
+
+
+drop table if exists news;
+create table news(
+	id SERIAL PRIMARY KEY,
+	news_text TEXT NOT NULL,
+	serials_id BIGINT UNSIGNED,	
+	foreign key (serials_id) references serials(id) ON DELETE set null ON UPDATE CASCADE
+);
+
+
+drop table if exists likes;
+create table likes(
+	user_id BIGINT UNSIGNED NOT NULL,
+	serials_id BIGINT UNSIGNED NOT NULL,
+	raiting_a ENUM ('1', '2', '3', '4', '5' ,'6', '7', '8', '9', '10'),
+	primary key (user_id, serials_id),
+	foreign key (user_id) references users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	foreign key (serials_id) references serials(id) ON DELETE CASCADE ON UPDATE CASCADE
+)COMMENT = 'оценки сериалов';
+
+drop table if exists seasons;
+create table seasons(
+	id SERIAL PRIMARY KEY,
+	serials_id BIGINT UNSIGNED NOT NULL,
+	seasons_id BIGINT UNSIGNED NOT NULL,
+	num int UNSIGNED not null,
+	data_start DATETIME,
+	seasons_text TEXT,
+	allepisods int UNSIGNED,
+	UNIQUE (serials_id, seasons_id),
+	foreign key (serials_id) references serials(id) ON DELETE CASCADE ON UPDATE CASCADE
+)COMMENT = 'сезоны';
+
+drop table if exists seria;
+create table seria(
+	id SERIAL PRIMARY KEY,
+	seasons_id BIGINT UNSIGNED NOT NULL,
+	seria_id BIGINT UNSIGNED NOT NULL,
+	num int UNSIGNED not null,
+	data_start DATETIME,
+	name VARCHAR(255),
+	UNIQUE (seasons_id, seria_id),
+	foreign key (seasons_id) references seasons(id) ON DELETE CASCADE ON UPDATE CASCADE
+)COMMENT = 'серии';
+
+drop table if exists likes_b;
+create table likes_b(
+	user_id BIGINT UNSIGNED NOT NULL,
+	seria_id BIGINT UNSIGNED NOT NULL,
+	raiting_b CHAR(1),
+	Primary key (user_id, seria_id),
+	foreign key (user_id) references users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	foreign key (seria_id) references seria(id) ON DELETE CASCADE ON UPDATE CASCADE	
+)COMMENT = 'лайки серий';
+
+drop table if exists schedule_serials;
+create table schedule_serials(
+	 user_id BIGINT UNSIGNED NOT NULL,
+	 serials_id BIGINT UNSIGNED NOT NULL,
+	 status ENUM('смотрю', 'посмотрела', 'бросила просмотр', 'приостановила просмотр', 'планирую посмотреть') COMMENT 'статус просмотра сериала',
+	 massege TEXT,
+	 Primary key (user_id, serials_id),
+	 foreign key (user_id) references users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	 foreign key (serials_id) references serials(id) ON DELETE CASCADE ON UPDATE CASCADE
+) COMMENT = 'статус просмотр по каждому сериалу';
+
+drop table if exists views;
+create table views(
+	user_id BIGINT UNSIGNED NOT NULL,
+	seria_id BIGINT UNSIGNED NOT NULL,
+	view CHAR(1),
+	Primary key (user_id, seria_id),
+	foreign key (user_id) references users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	foreign key (seria_id) references seria(id) ON DELETE CASCADE ON UPDATE CASCADE	
+)COMMENT = 'лайки серий';
+
+
 
 
 
